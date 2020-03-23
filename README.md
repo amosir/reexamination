63a8a47adc4629558972f855cfdde6d5e58a3975
tiansir <1289131800@qq.com>
2020-03-23 15:30:12 +0800

    最简真分数

--- README.md
+++ README.md
@@ -19,3 +19,5 @@
 17. [剩下的树](https://github.com/Tiansir-wg/reexamination/blob/master/lefttree.c)
 18. [查找学生信息](https://github.com/Tiansir-wg/reexamination/blob/master/findStuInfo.c)
 19. [谁是你的潜在朋友](https://github.com/Tiansir-wg/reexamination/blob/master/latentFriends.c)
+20. [密码翻译](https://github.com/Tiansir-wg/reexamination/blob/master/codeTranslation.c)
+21. [最简真分数](https://github.com/Tiansir-wg/reexamination/blob/master/simplestFraction.cpp)
--- simplestFraction.cpp
+++ simplestFraction.cpp
@@ -1,46 +1,49 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-int gcd(int x, int y)
+#include <stdio.h>
+#define MAX_SIZE 600
+int gcd(int num1, int num2)
 {
-    while (y != 0)
+    // num1始终大于num2
+    if (num2 > num1)
     {
-
-        int temp = x % y;
-        x = y;
-        y = temp;
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
     }
-    return x;
+    while (num2 != 0)
+    {
+        int temp = num1 % num2;
+        num1 = num2;
+        num2 = temp;
+    }
+    return num1;
 }
 int main()
 {
     int cnt;
-    while (cin >> cnt)
-    {
+    scanf("%d", &cnt);
 
-        int total = 0;
-        int num[cnt];
-        for (int i = 0; i < cnt; i++)
+    int nums[MAX_SIZE];
+    for (int i = 0; i < cnt; i++)
+    {
+        scanf("%d", &nums[i]);
+    }
+
+    int total = 0;
+    for (int i = 0; i < cnt; i++)
+    {
+        for (int j = 0; j < cnt; j++)
         {
-            int temp;
-            cin >> temp;
-            num[i] = temp;
-        }
-        for (int i = 0; i < cnt; i++)
-        {
-            for (int j = 0; j < cnt; j++)
+            if (nums[i] == nums[j])
             {
-                if (num[i] == num[j])
-                {
-                    continue;
-                }
-                if (num[i] > num[j] && gcd(num[i], num[j]) == 1)
-                {
-                    total++;
-                }
+                continue;
+            }
+            // 真分数且最简
+            if (nums[i] > nums[j] && gcd(nums[i], nums[j]) == 1)
+            {
+                total++;
             }
         }
-        cout << total << endl;
     }
+    printf("%d\n", total);
     return 0;
 }
