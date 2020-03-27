65ac3e84f4cc54de0a7aac3682f8f99538da99a1
tiansir <1289131800@qq.com>
2020-03-27 15:40:43 +0800

    守形数

--- guardNumber.c
+++ guardNumber.c
@@ -1,1 +1,35 @@
 #include <stdio.h>
+int main()
+{
+    int num;
+    while (scanf("%d", &num) != EOF)
+    {
+        // 是否是守形数
+        int flag = 1;
+        // 平方
+        int squareOfNum = num * num;
+        while (num != 0)
+        {
+            if (num % 10 != squareOfNum % 10)
+            {
+                // 不是守形数
+                flag = 0;
+                break;
+            }
+            else
+            {
+                num /= 10;
+                squareOfNum /= 10;
+            }
+        }
+        if (flag == 0)
+        {
+            printf("No!");
+        }
+        else
+        {
+            printf("Yes!");
+        }
+    }
+    return 0;
+}
